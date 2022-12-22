86b14b70abfe3b2619ae0149f434344aaf86e74e
rChrisb <5767@holbertonstudents.com>
2022-12-22 16:21:07 +0100

    JS file

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-console.log("Il a pas dit \"Bonjour\"")
-console.log("Du coup, il s'est fait niquer sa mère!!")
+// console.log("Il a pas dit \"Bonjour\"")
+// console.log("Du coup, il s'est fait niquer sa mère!!")
 
-// alert("LETSGOOOO")
-currentYear = 2022;
-console.log(typeof currentYear)
-console.log(typeof "Hello")
-console.log(typeof 'Hello')
+// // alert("LETSGOOOO")
+// currentYear = 2022;
+// console.log(typeof currentYear)
+// console.log(typeof "Hello")
+// console.log(typeof 'Hello')
 
-let currentMonth;
-console.log(typeof currentMonth)
+// let currentMonth;
+// console.log(typeof currentMonth) /* undefined */
+
+// const myName = "BIKOY"
+// // name = "KOYBI" typeError: Assignment to constant variable.
+// surname = "KOYBI";
+// console.log(surname + ' '+ myName)
+// let n = 1000 * 32;
+// console.log(n)
+// n %= 15;
+// console.log(n)
+// console.log(32000 % 15) // same result as n %=15
+// console.log(currentYear < 2000) /*prints either 'true' or 'false" (currentYear is 2022 in line 5)*/
