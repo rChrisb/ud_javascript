f33873a898ef07266e08f2ae602859ad4005714f
rChrisb <5767@holbertonstudents.com>
2022-12-22 15:33:15 +0100

    JS file

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("Il a pas dit 'Bonjour'")
+console.log("Il a pas dit \"Bonjour\"")
 console.log("Du coup, il s'est fait niquer sa mère!!")
 
 // alert("LETSGOOOO")
@@ -6,3 +6,6 @@ currentYear = 2022;
 console.log(typeof currentYear)
 console.log(typeof "Hello")
 console.log(typeof 'Hello')
+
+let currentMonth;
+console.log(typeof currentMonth)
