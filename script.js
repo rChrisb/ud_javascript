a2b114b46695c560dbeaaa81ef97c2e01759b897
rChrisb <5767@holbertonstudents.com>
2022-12-22 16:59:42 +0100

    JS file

--- script.js
+++ script.js
@@ -20,3 +20,31 @@
 // console.log(n)
 // console.log(32000 % 15) // same result as n %=15
 // console.log(currentYear < 2000) /*prints either 'true' or 'false" (currentYear is 2022 in line 5)*/
+
+//TEST 1
+let heightMark, weightMark, heightJohn, weightJohn;
+heightMark = 1.69;
+weightMark = 78;
+heightJohn = 1.95;
+weightJohn = 92;
+
+let bmiMark = weightMark/heightMark**2
+console.log(bmiMark)
+let bmiJohn = weightJohn/heightJohn**2
+console.log(bmiJohn)
+let markHigherBMI = bmiMark > bmiJohn
+console.log(markHigherBMI)
+
+//TEST2 (changing Mark's and John's DATAS)
+heightMark = 1.88;
+weightMark = 95;
+heightJohn = 1.76;
+weightJohn = 85;
+
+bmiMark = weightMark/heightMark**2;
+console.log(bmiMark)
+bmiJohn = weightJohn/heightJohn**2
+console.log(bmiJohn)
+
+markHigherBMI = bmiMark > bmiJohn
+console.log(markHigherBMI)
