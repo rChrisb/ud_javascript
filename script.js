84169c3e3593412e07a7e9d427ddf66906248eb5
rChrisb <5767@holbertonstudents.com>
2022-12-22 17:51:21 +0100

    JS file

--- script.js
+++ script.js
@@ -22,21 +22,21 @@
 // console.log(currentYear < 2000) /*prints either 'true' or 'false" (currentYear is 2022 in line 5)*/
 
 //TEST 1
-let heightMark, weightMark, heightJohn, weightJohn;
-heightMark = 1.69;
-weightMark = 78;
-heightJohn = 1.95;
-weightJohn = 92;
+// let heightMark, weightMark, heightJohn, weightJohn;
+// heightMark = 1.69;
+// weightMark = 78;
+// heightJohn = 1.95;
+// weightJohn = 92;
 
-let bmiMark = weightMark/heightMark**2
-console.log(bmiMark)
-let bmiJohn = weightJohn/heightJohn**2
-console.log(bmiJohn)
-let markHigherBMI = bmiMark > bmiJohn
-console.log(markHigherBMI)
+// let bmiMark = weightMark/heightMark**2
+// console.log(bmiMark)
+// let bmiJohn = weightJohn/heightJohn**2
+// console.log(bmiJohn)
+// let markHigherBMI = bmiMark > bmiJohn
+// console.log(markHigherBMI)
 
 //TEST2 (changing Mark's and John's DATAS)
-heightMark = 1.88;
+/*heightMark = 1.88;
 weightMark = 95;
 heightJohn = 1.76;
 weightJohn = 85;
@@ -47,4 +47,22 @@ bmiJohn = weightJohn/heightJohn**2
 console.log(bmiJohn)
 
 markHigherBMI = bmiMark > bmiJohn
-console.log(markHigherBMI)
+console.log(markHigherBMI)*/
+
+// console.log("hello\nguys")
+// console.log("hello\n\
+// dude")
+// console.log(`hello
+// guys`)
+
+// let first = "chris", second = "rubz", third = "biks";
+// let id = `Hello ${first}, my name is ${second} ${third}`;
+// console.log(id)
+let age = 19;
+const majorAge = age >= 18;
+if (majorAge) {
+	console.log(`I'm ${age} so I can drink as much as I want!!`);
+}
+else {
+	console.log(`I am only ${age}, so my parent won't let me drink`);
+}
