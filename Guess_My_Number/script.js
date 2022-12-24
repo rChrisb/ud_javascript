5fcfef85db685d72a77722e759631407d32e15e7
rChrisb <5767@holbertonstudents.com>
2022-12-24 18:00:33 +0100

    JS file

---
+++ Guess_My_Number/script.js
@@ -0,0 +1,6 @@
+"use strict";
+
+// document.querySelector(".guess").value = "22";
+document.querySelector(".check").addEventListener("click", function () {
+  console.log(document.querySelector(".guess").value);
+});
