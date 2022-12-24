290c0b4c408476d0578d0971c808a1ded3e2baa6
rChrisb <5767@holbertonstudents.com>
2022-12-24 18:17:08 +0100

    JS file

--- Guess_My_Number/script.js
+++ Guess_My_Number/script.js
@@ -4,4 +4,10 @@
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   console.log(guess);
+  if (!guess)
+    document.querySelector(".message").textContent =
+      "You have to type a guess number!";
+  else if (guess == 12)
+    document.querySelector(".message").textContent = "CONGRATULATIONS";
+  else document.querySelector(".message").textContent = "No! Guess again";
 });
