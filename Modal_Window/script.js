def7cdc2492bc7c9676a9f88d7a2e121991f22f2
rChrisb <5767@holbertonstudents.com>
2022-12-25 17:13:15 +0100

    JavaScript file

--- Modal_Window/script.js
+++ Modal_Window/script.js
@@ -1,5 +1,5 @@
 "use strict";
-const moadal = document.querySelector(".modal");
+const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const buttonCloseModal = document.querySelector(".close-modal");
 const buttonsOpenModal = document.querySelectorAll(".show-modal");
@@ -8,3 +8,15 @@ console.log(buttonsOpenModal);
 for (let i = 0; i < buttonsOpenModal.length; i++) {
   console.log(buttonsOpenModal[i].textContent);
 }
+for (let i = 0; i < buttonsOpenModal.length; i++) {
+  buttonsOpenModal[i].addEventListener("click", function () {
+    /*   document.querySelector(".hidden").style.display = "inline";
+  });
+}*/
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  });
+}
+buttonCloseModal.addEventListener("click", function () {
+  modal.classList.add("hidden");
+});
