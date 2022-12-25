217275a9374ff02050127d5e0512e79297e528a1
rChrisb <5767@holbertonstudents.com>
2022-12-25 13:35:43 +0100

    JS file

--- Guess_My_Number/script.js
+++ Guess_My_Number/script.js
@@ -9,22 +9,38 @@ document.querySelector(".number").textContent = secret; // random num between 0 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   console.log(guess);
+
+  //when no answer
   if (!guess)
     document.querySelector(".message").textContent =
       "You have to type a guess number!";
-  else if (guess === secret)
+  //when right answer
+  else if (guess === secret) {
     document.querySelector(".message").textContent = "CONGRATULATIONS";
+    document.querySelector("body").style.backgroundColor = "#60b347";
+  }
+  //when answer is too low
   else if (guess < secret) {
-    if (score > 0) {
+    if (score > 1) {
       document.querySelector(".message").textContent = "Your guess is too low";
       score--;
       document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent =
+        "You lost bro! it's OVER";
+      document.querySelector(".score").textContent = 0;
     }
+
+    //when answer is too high
   } else if (guess > secret) {
-    if (score > 0) {
+    if (score > 1) {
       document.querySelector(".message").textContent = "Your guess is too high";
       score--;
       document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent =
+        "You lost bro! it's OVER";
+      document.querySelector(".score").textContent = 0;
     }
   }
   document.querySelector(".highscore").textContent = score;
