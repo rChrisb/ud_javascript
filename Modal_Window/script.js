be60d519d1aa3f16ba2795e6bff816c05dffa9c4
rChrisb <5767@holbertonstudents.com>
2022-12-25 16:45:00 +0100

    JavaScript file

--- Modal_Window/script.js
+++ Modal_Window/script.js
@@ -1,1 +1,10 @@
 "use strict";
+const moadal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const buttonCloseModal = document.querySelector(".close-modal");
+const buttonsOpenModal = document.querySelectorAll(".show-modal");
+console.log(buttonsOpenModal);
+
+for (let i = 0; i < buttonsOpenModal.length; i++) {
+  console.log(buttonsOpenModal[i].textContent);
+}
