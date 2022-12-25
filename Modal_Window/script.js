466c22a74443025cf813b579c3c95de3431281b9
rChrisb <5767@holbertonstudents.com>
2022-12-25 17:49:55 +0100

    JavaScript file

--- Modal_Window/script.js
+++ Modal_Window/script.js
@@ -3,20 +3,32 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const buttonCloseModal = document.querySelector(".close-modal");
 const buttonsOpenModal = document.querySelectorAll(".show-modal");
-console.log(buttonsOpenModal);
 
+// console.log(buttonsOpenModal);
+
+const openingModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+// for (let i = 0; i < buttonsOpenModal.length; i++) {
+//   console.log(buttonsOpenModal[i].textContent);
+// }
 for (let i = 0; i < buttonsOpenModal.length; i++) {
-  console.log(buttonsOpenModal[i].textContent);
+  buttonsOpenModal[i].addEventListener("click", openingModal);
 }
-for (let i = 0; i < buttonsOpenModal.length; i++) {
-  buttonsOpenModal[i].addEventListener("click", function () {
-    /*   document.querySelector(".hidden").style.display = "inline";
-  });
-}*/
-    modal.classList.remove("hidden");
-    overlay.classList.remove("hidden");
-  });
-}
-buttonCloseModal.addEventListener("click", function () {
+
+const closingMod = function () {
   modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+buttonCloseModal.addEventListener("click", closingMod);
+overlay.addEventListener("click", closingMod);
+
+document.addEventListener("keydown", function (event) {
+  console.log(event.key); // event(function parameter) is an object
+  if (event.key === "Escape" && !modal.classList.contains("hidden")) {
+    closingMod();
+  }
 });
