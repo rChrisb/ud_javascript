9b16d016c46cc9a65ffffb7a0aef0623ede09ee5
rChrisb <5767@holbertonstudents.com>
2022-12-25 20:15:41 +0100

    JavaScript file

--- Pig_Game/script.js
+++ Pig_Game/script.js
@@ -1,5 +1,8 @@
 "use strict";
 // Selecting elements
+const player0_element = document.querySelector(".player--0");
+const player1_element = document.querySelector(".player--1");
+
 const score0_element = document.querySelector("#score--0");
 const score1_element = document.getElementById("score--1");
 
@@ -18,22 +21,58 @@ dice_element.classList.add("hidden");
 const scores = [0, 0];
 let currentScore = 0;
 let active_player = 0;
+let playing = true;
+
+const switchPlayer = function () {
+  document.getElementById(`current--${active_player}`).textContent = 0;
+  active_player = active_player === 0 ? 1 : 0;
+  currentScore = 0;
+  player0_element.classList.toggle("player--active");
+  player1_element.classList.toggle("player--active");
+};
 
 // Rolling dice functionnality
 buttonRoll.addEventListener("click", function () {
-  // 1. Genrating a random dice roll
-  const dice = Math.trunc(Math.random() * 6) + 1;
-  // 2. Display dice
-  dice_element.classList.remove("hidden");
-  dice_element.src = `dice-${dice}.png`;
+  if (playing) {
+    // 1. Genrating a random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    // 2. Display dice
+    dice_element.classList.remove("hidden");
+    dice_element.src = `dice-${dice}.png`;
 
-  // 3. Check for rolled 1
-  if (dice !== 1) {
-    currentScore += dice;
-    document.getElementById(`current--${active_player}`).textContent =
-      currentScore;
-  } else {
-    // Switch to next player
-    active_player = active_player === 0 ? 1 : 0;
+    // 3. Check for rolled 1
+    if (dice !== 1) {
+      currentScore += dice;
+      document.getElementById(`current--${active_player}`).textContent =
+        currentScore;
+    } else {
+      // Switch to next player
+      switchPlayer();
+    }
+  }
+});
+
+buttonHold.addEventListener("click", function () {
+  if (playing) {
+    // 1. Add current score to active player(s score
+    scores[active_player] += currentScore;
+    document.getElementById(`score--${active_player}`).textContent =
+      scores[active_player];
+
+    // 2. Check if player's score is <= 100
+    if (scores[active_player] >= 20) {
+      //Finish the game
+      playing = false;
+      document
+        .querySelector(`.player--${active_player}`)
+        .classList.add("player--winner");
+      document
+        .querySelector(`.player--${active_player}`)
+        .classList.remove("player--active");
+      dice_element.classList.add("hidden");
+    } else {
+      // Switch to the next player
+      switchPlayer();
+    }
   }
 });
