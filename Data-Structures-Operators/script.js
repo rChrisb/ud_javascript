642f75375b02bb0b6c4c542ed726940d30e96476
rChrisb <5767@holbertonstudents.com>
2022-12-27 20:49:59 +0100

    JS file

--- Data-Structures-Operators/script.js
+++ Data-Structures-Operators/script.js
@@ -9,3 +9,18 @@ console.log(b, c, "same?");
 const tab = [45, 87, [87, 89, 85], 79];
 const [n, , [, x], y] = tab;
 console.log(n, x, y);
+
+arra.unshift(101, 102);
+console.log(arra);
+console.log(...arra);
+
+const fun = function (a, b, c) {
+  console.log(`${(a + b + c) / 3}`);
+};
+const moy = [6, 8, 13];
+fun(...moy);
+const a = [...moy];
+console.log(a);
+console.log(...moy);
+const [f, r, s] = [...moy];
+console.log(r);
