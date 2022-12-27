967da4e05df477d8d36a5eb3628be0c0269ec11a
rChrisb <5767@holbertonstudents.com>
2022-12-27 21:13:58 +0100

    JS file

--- Data-Structures-Operators/script.js
+++ Data-Structures-Operators/script.js
@@ -1,26 +1,42 @@
 "use strict";
-const arra = [1, 5, 6, 7];
-const [i, , , k] = arra;
-console.log(i, k);
-let [, b, c] = arra;
-[c, b] = [b, c];
-console.log(b, c, "same?");
+// const arra = [1, 5, 6, 7];
+// const [i, , , k] = arra;
+// console.log(i, k);
+// let [, b, c] = arra;
+// [c, b] = [b, c];
+// console.log(b, c, "same?");
 
-const tab = [45, 87, [87, 89, 85], 79];
-const [n, , [, x], y] = tab;
-console.log(n, x, y);
+// const tab = [45, 87, [87, 89, 85], 79];
+// const [n, , [, x], y] = tab;
+// console.log(n, x, y);
 
-arra.unshift(101, 102);
-console.log(arra);
-console.log(...arra);
+// arra.unshift(101, 102);
+// console.log(arra);
+// console.log(...arra);
 
-const fun = function (a, b, c) {
-  console.log(`${(a + b + c) / 3}`);
+// const fun = function (a, b, c) {
+//   console.log(`${(a + b + c) / 3}`);
+// };
+// const moy = [6, 8, 13];
+// fun(...moy);
+// const a = [...moy];
+// console.log(a);
+// console.log(...moy);
+// const [f, r, s] = [...moy];
+// console.log(r);
+
+// const [z, ...others] = arra;
+// console.log(z, others, arra);
+// console.log([...others, ...arra]);
+
+const add = function (...numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) sum += numbers[i];
+  console.log(sum);
 };
-const moy = [6, 8, 13];
-fun(...moy);
-const a = [...moy];
-console.log(a);
-console.log(...moy);
-const [f, r, s] = [...moy];
-console.log(r);
+add(2, 4);
+add(2, 8, 12);
+add(20, 100, 12, 180, 13);
+
+const x = [24, 26, 50];
+add(...x);
