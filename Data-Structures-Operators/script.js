b963f757b1a8c8c52c1cd220c728f0b4339f062c
rChrisb <5767@holbertonstudents.com>
2022-12-27 19:43:48 +0100

    JS file

--- Data-Structures-Operators/script.js
+++ Data-Structures-Operators/script.js
@@ -1,1 +1,11 @@
 "use strict";
+const arra = [1, 5, 6, 7];
+const [i, , , k] = arra;
+console.log(i, k);
+let [, b, c] = arra;
+[c, b] = [b, c];
+console.log(b, c, "same?");
+
+const tab = [45, 87, [87, 89, 85], 79];
+const [n, , [, x], y] = tab;
+console.log(n, x, y);
