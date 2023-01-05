4a031522b1c2f4a5b4ccb321b9396039e5b29d9e
rChrisb <5767@holbertonstudents.com>
2023-01-05 20:39:48 +0100

    JS file

--- Advanced_DOM_and_Events_Bankist/script.js
+++ Advanced_DOM_and_Events_Bankist/script.js
@@ -1,1 +1,51 @@
 "use strict";
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".btn--close-modal");
+const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+
+const openModal = function (e) {
+  e.preventDefault();
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+btnsOpenModal.forEach((btn) => btn.addEventListener("click", openModal));
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
+
+const message = document.createElement("div");
+message.classList.add("cookie-message");
+const header = document.querySelector(".header");
+message.innerHTML =
+  'Cookie <button class= "btn btn--close-cookie"> Yeup!</button>';
+header.append(message);
+message.style.width = "120%";
+
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
+
+btnScrollTo.addEventListener("click", function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+
+  /*  window.scrollTo({
+    left: s1coords.left + window.pageXOffset,
+    top: s1coords.top + window.pageYOffset,
+    behavior: "smooth",
+  }); */
+  section1.scrollIntoView({ behavior: "smooth" });
+});
