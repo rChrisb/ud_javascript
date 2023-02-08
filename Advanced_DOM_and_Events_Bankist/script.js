b25531d3c288c357aeb548ad0e26631619b3e9ff
rChrisb <5767@holbertonstudents.com>
2023-02-08 14:12:37 +0100

    Passing Arguments to Event Handlers

--- Advanced_DOM_and_Events_Bankist/script.js
+++ Advanced_DOM_and_Events_Bankist/script.js
@@ -4,6 +4,10 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
+const nav = document.querySelector(".nav");
 
 const openModal = function (e) {
   e.preventDefault();
@@ -41,20 +45,17 @@ document.addEventListener("keydown", function (e) {
 document.querySelector(".nav__links").addEventListener("click", function (e) {
   if (e.target.classList.contains("nav__link")) {
     e.preventDefault();
-    this.style.backgroundColor = randomColor();
+    /* this.style.backgroundColor = randomColor(); */
 
     const id = e.target.getAttribute("href");
     console.log(id);
     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
   }
 });
-const tabs = document.querySelectorAll(".operations__tab");
-const tabsContainer = document.querySelector(".operations__tab-container");
-const tabsContent = document.querySelectorAll(".operations__content");
 
 tabsContainer.addEventListener("click", function (e) {
   const clicked = e.target.closest(".operations__tab");
-  console.log(clicked);
+  /*  console.log(clicked); */
 
   if (!clicked) return;
   tabs.forEach((t) => t.classList.remove("operations__tab--active"));
@@ -66,6 +67,21 @@ tabsContainer.addEventListener("click", function (e) {
     .classList.add(`operations__content--active`);
 });
 
+const handleHover = function (e) {
+  if (e.target.classList.contains("nav__link")) {
+    const link = e.target;
+    const siblings = link.closest(".nav").querySelectorAll(".nav__link");
+    const logo = link.closest(".nav").querySelector("img");
+
+    siblings.forEach((el) => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+
+nav.addEventListener("mouseover", handleHover.bind(0.5));
+nav.addEventListener("mouseout", handleHover.bind(1));
 // const message = document.createElement("div");
 // message.classList.add("cookie-message");
 // const header = document.querySelector(".header");
