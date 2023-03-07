5a89a2e774d5f016ef644fe4a3be17a9c7f0ab5d
rChrisb <5767@holbertonstudents.com>
2023-03-07 16:23:53 +0100

    Building a Slider Component

--- Advanced_DOM_and_Events_Bankist/script.js
+++ Advanced_DOM_and_Events_Bankist/script.js
@@ -158,35 +158,77 @@ imgTargets.forEach((img) => imgObserver.observe(img));
 const slides = document.querySelectorAll(".slide");
 const btnLeft = document.querySelector(".slider__btn--left");
 const btnRight = document.querySelector(".slider__btn--right");
-let currentSlide = 0;
-const numberOfSlides = slides.length;
-
+const dotContainer = document.querySelector(".dots");
 const slider = document.querySelector(".slider");
 
 slider.style.overflow = "visible";
 
+let currentSlide = 0;
+const numberOfSlides = slides.length;
+
+const createDots = function () {
+  slides.forEach(function (_, i) {
+    dotContainer.insertAdjacentHTML(
+      "beforeend",
+      `<button class="dots__dot" data-slide="${i}"></button>`
+    );
+  });
+};
+
+const activateDot = function (slide) {
+  document
+    .querySelectorAll(".dots__dot")
+    .forEach((dot) => dot.classList.remove("dots__dot--active"));
+
+  document
+    .querySelector(`.dots__dot[data-slide="${slide}"]`)
+    .classList.add("dots__dot--active");
+};
 const goToSlide = function (slide) {
   slides.forEach(
     (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
   );
 };
-goToSlide(0);
 
-btnRight.addEventListener("click", function () {
+const nextSlide = function () {
   if (currentSlide === numberOfSlides - 1) {
     currentSlide = 0;
   } else currentSlide++;
 
   goToSlide(currentSlide);
-});
-btnLeft.addEventListener("click", function () {
+  activateDot(currentSlide);
+};
+
+const prevSlide = function () {
   if (currentSlide === 0) {
     currentSlide = numberOfSlides - 1;
   } else currentSlide--;
 
   goToSlide(currentSlide);
+  activateDot(currentSlide);
+};
+const init = function () {
+  goToSlide(0);
+  createDots();
+  activateDot(0);
+};
+init();
+
+btnRight.addEventListener("click", nextSlide);
+btnLeft.addEventListener("click", prevSlide);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") prevSlide();
+  if (e.key === "ArrowRight") nextSlide();
 });
 
+dotContainer.addEventListener("click", function (e) {
+  if (e.target.classList.contains("dots__dot")) {
+    const { slide } = e.target.dataset;
+    goToSlide(slide);
+    activateDot(currentSlide);
+  }
+});
 // const message = document.createElement("div");
 // message.classList.add("cookie-message");
 // const header = document.querySelector(".header");
