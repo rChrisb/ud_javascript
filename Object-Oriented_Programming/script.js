10aabad629d75b18ccc7c7b9731fd59e8f60fb4f
rChrisb <5767@holbertonstudents.com>
2023-03-08 15:20:11 +0100

    Constructor functions and the 'new' operator

--- Object-Oriented_Programming/script.js
+++ Object-Oriented_Programming/script.js
@@ -1,15 +1,65 @@
 "use strict";
 
-const Player = function (team, number, position) {
-  this.team = team;
-  this.number = number;
-  this.position = position;
+// const Player = function (team, number, position, lastName) {
+//   this.team = team;
+//   this.number = number;
+//   this.position = position;
+//   this.lastName = lastName;
+// };
+
+// const messi = new Player("Barça", 10, "RW", "Messi");
+// console.log(messi);
+// console.log(messi.team);
+
+// messi.team = "PSG";
+// messi.number = 30;
+// console.log(messi);
+
+// Player.prototype.info = function () {
+//   console.log(
+//     `${this.lastName} plays for ${this.team} and wears ${this.number}`
+//   );
+// };
+// // messi.info = function () {
+// //   console.log(
+// //     `${this.lastName} plays for ${this.team} and wears ${this.number}`
+// //   );
+// // };
+// messi.info();
+// console.log(messi);
+// console.dir(messi);
+
+// const arr = [1, 1, 2, 2, 2, 3, 3, 3, 3];
+// console.log([...new Set(arr)]);
+
+// Coding Challenge #1
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
 };
 
-const messi = new Player("Barça", 10, "RW");
-console.log(messi);
-console.log(messi.team);
+Car.prototype.accelerate = function () {
+  this.speed = this.speed + 10;
+  console.log(`${this.make}'s speed has been increased up to ${this.speed}`);
+};
+Car.prototype.brake = function () {
+  this.speed = this.speed - 5;
+  console.log(`${this.make}'s speed has been decreased down to ${this.speed}`);
+};
 
-messi.team = "PSG";
-messi.number = 30;
-console.log(messi);
+const mercedes = new Car("Mercedes", 120);
+const peugeot = new Car("Peugeot", 95);
+
+mercedes.accelerate();
+mercedes.accelerate();
+mercedes.accelerate();
+mercedes.brake();
+mercedes.brake();
+mercedes.brake();
+peugeot.accelerate();
+peugeot.accelerate();
+peugeot.accelerate();
+peugeot.brake();
+peugeot.brake();
+peugeot.brake();
