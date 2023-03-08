ec68557ea104e2af2b6c3c40f83711f8469a4743
rChrisb <5767@holbertonstudents.com>
2023-03-08 15:49:47 +0100

    ES6 Classes

--- Object-Oriented_Programming/script.js
+++ Object-Oriented_Programming/script.js
@@ -34,32 +34,48 @@
 
 // Coding Challenge #1
 
-const Car = function (make, speed) {
-  this.make = make;
-  this.speed = speed;
-};
+// const Car = function (make, speed) {
+//   this.make = make;
+//   this.speed = speed;
+// };
 
-Car.prototype.accelerate = function () {
-  this.speed = this.speed + 10;
-  console.log(`${this.make}'s speed has been increased up to ${this.speed}`);
-};
-Car.prototype.brake = function () {
-  this.speed = this.speed - 5;
-  console.log(`${this.make}'s speed has been decreased down to ${this.speed}`);
-};
+// Car.prototype.accelerate = function () {
+//   this.speed = this.speed + 10;
+//   console.log(`${this.make}'s speed has been increased up to ${this.speed}`);
+// };
+// Car.prototype.brake = function () {
+//   this.speed = this.speed - 5;
+//   console.log(`${this.make}'s speed has been decreased down to ${this.speed}`);
+// };
 
-const mercedes = new Car("Mercedes", 120);
-const peugeot = new Car("Peugeot", 95);
+// const mercedes = new Car("Mercedes", 120);
+// const peugeot = new Car("Peugeot", 95);
 
-mercedes.accelerate();
-mercedes.accelerate();
-mercedes.accelerate();
-mercedes.brake();
-mercedes.brake();
-mercedes.brake();
-peugeot.accelerate();
-peugeot.accelerate();
-peugeot.accelerate();
-peugeot.brake();
-peugeot.brake();
-peugeot.brake();
+// mercedes.accelerate();
+// mercedes.accelerate();
+// mercedes.accelerate();
+// mercedes.brake();
+// mercedes.brake();
+// mercedes.brake();
+// peugeot.accelerate();
+// peugeot.accelerate();
+// peugeot.accelerate();
+// peugeot.brake();
+// peugeot.brake();
+// peugeot.brake();
+
+class Player {
+  constructor(name, team, number) {
+    this.name = name;
+    this.team = team;
+    this.number = number;
+  }
+
+  information() {
+    console.log(`${this.name} plays for ${this.team} and wears ${this.number}`);
+  }
+}
+
+const neymar = new Player("Neymar", "PSG", 10);
+console.log(neymar);
+neymar.information();
