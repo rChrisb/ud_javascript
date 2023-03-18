733c99334a685137991493f0b4a66b12389857ea
rChrisb <5767@holbertonstudents.com>
2023-03-18 14:28:53 +0100

    thriwing errors manually

--- Asynchronous_JavaScript/script.js
+++ Asynchronous_JavaScript/script.js
@@ -113,16 +113,46 @@ const renderError = function (msg) {
 //       renderCountry(data[0]);
 //     });
 // };
+const getJSON = function (url, errorMsg = "Sum went left") {
+  return fetch(url).then((response) => {
+    if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+    return response.json();
+  });
+};
 
+// const getCountryData = function (country) {
+//   fetch(`https://restcountries.com/v3.1/name/${country}`)
+//     .then((response) => {
+//       if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+//       return response.json();
+//     })
+//     .then((data) => {
+//       renderCountry(data[0]);
+//       const neighbour = data[0].borders?.[0];
+//       return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+//     })
+//     .then((response) => response.json())
+//     .then((data) => renderCountry(data[0], "neighbour"))
+//     .catch((err) => {
+//       console.error(`${err} :(`);
+//       renderError(`yoo sum went left broda: ${err.message}`);
+//     })
+//     .finally(() => (countriesContainer.style.opacity = 1));
+// };
 const getCountryData = function (country) {
-  fetch(`https://restcountries.com/v3.1/name/${country}`)
-    .then((response) => response.json())
+  getJSON(
+    `https://restcountries.com/v3.1/name/${country}`,
+    `Cannot find country`
+  )
     .then((data) => {
       renderCountry(data[0]);
       const neighbour = data[0].borders?.[0];
-      return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+      if (!neighbour) throw new Error(`${country} has no neighbour`);
+      return getJSON(
+        `https://restcountries.com/v3.1/alpha/${neighbour}`,
+        "Cannot find neighbour country"
+      );
     })
-    .then((response) => response.json())
     .then((data) => renderCountry(data[0], "neighbour"))
     .catch((err) => {
       console.error(`${err} :(`);
@@ -132,5 +162,6 @@ const getCountryData = function (country) {
 };
 
 btn.addEventListener("click", function () {
-  getCountryData("cafre");
+  getCountryData("australia");
 });
+console.log("okkk");
